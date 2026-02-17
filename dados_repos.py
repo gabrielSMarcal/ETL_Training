646d5fb11b48fdc7ed6132ad28fab32c952db367
GabrielSMarcal <gabriel.santos.marcal>
2026-02-17 09:56:35 -0300

    Criação de classe para estruturação do DF, desde o fetch do repositório dos usuários, até a montagem da tabela de dados

---
+++ dados_repos.py
@@ -0,0 +1,68 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+import requests
+
+load_dotenv()
+
+class DadosRepositorios:
+    
+    def __init__(self, owner):
+        self.owner = owner
+        self.api_base_url = 'https://api.github.com'
+        self.auth_token = os.getenv('GITHUB_ACCESS_TOKEN')
+        self.headers = {
+            'Authorization': 'Bearer ' + self.auth_token,
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+    
+    def lista_repositorios(self):
+        lista_repos = []
+        
+        for page_num in range(1, 30):
+            try:
+                url = f'{self.api_base_url}/users/{self.owner}/repos?page={page_num}'
+                response = requests.get(url, headers=self.headers)
+                lista_repos.append(response.json())
+            except:
+                lista_repos.append(None)
+        
+        return lista_repos
+    
+    def nomes_repos(self, repos_list):
+        nomes_repos = []
+        
+        for page in repos_list:
+            if page is not None:
+                try:
+                    for repo in page:
+                        nomes_repos.append(repo['name'])
+                except:
+                    pass
+                
+        return nomes_repos
+    
+    def nome_linguagens(self, repos_list):
+        repo_linguagens = []
+        
+        for page in repos_list:
+            if page is not None:
+                try:
+                    for repo in page:
+                        repo_linguagens.append(repo['language'])
+                except:
+                    pass
+                
+        return repo_linguagens
+    
+    def cria_df_linguagens(self):
+        
+        repositorios = self.lista_repositorios()
+        nomes = self.nomes_repos(repositorios)
+        linguagens = self.nome_linguagens(repositorios)
+        
+        dados = pd.DataFrame()
+        dados['nome_repositorio'] = nomes
+        dados['linguagem'] = linguagens
+        
+        return dados
